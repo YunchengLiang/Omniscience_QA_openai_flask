d6953ebff2b9ea3b07c58fbefc304d9cda612e30
YunchengLiang <yuncheng.liang@rci.rogers.ca>
2023-04-24 22:37:23 -0400

    day1

--- app.py
+++ app.py
@@ -1,162 +1,68 @@
-from flask import Flask, request, render_template, flash, redirect, url_for, send_file, Response
-from werkzeug.utils import secure_filename
-from datetime import datetime
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-import numpy as np
-import argparse
-import cv2
-import os
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text, func
+
 
 app = Flask(__name__)
 
-prototxtPath= os.path.join("face_detector","deploy.prototxt")
-weightsPath= os.path.join("face_detector","res10_300x300_ssd_iter_140000.caffemodel")
-net= cv2.dnn.readNet(prototxtPath, weightsPath)
-model= load_model('mask_detector.model')
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+USERNAME = 'root'
+PASSWORD = 'root'
+DATABASE = 'database_learn'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4'
 
-def detect_and_predict_mask(frame,faceNet,maskModel):
-    (h,w)= frame.shape[:2]
-    blob= cv2.dnn.blobFromImage(frame,1.0,(300,300),(104.0,177.0,123.0))#normalization
-    faceNet.setInput(blob)
-    detections= faceNet.forward()
+db=SQLAlchemy(app)
 
-    faces= []
-    locs= []
-    preds= []
+# with app.app_context():
+#     with db.engine.connect() as con:
+#         rs = con.execute(text("select 1"))
+#         print(rs.fetchone()[0])
 
-    for i in range(0,detections.shape[2]):
-        confidence= detections[0,0,i,2]
-        if confidence>0.5:
-            box= detections[0,0,i,3:7]*np.array([w,h,w,h])
-            (start_X,start_Y, end_X, end_Y)= box.astype('int')
-            (start_X,start_Y)= (max(0,start_X),max(0,start_Y))
-            (end_X,end_Y)= (min(w-1,end_X),min(h-1,end_Y))
-            face= frame[start_Y:end_Y, start_X:end_X]
-            face= cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
-            face= cv2.resize(face, (224,224))
-            face= img_to_array(face)
-            face= preprocess_input(face)
-            faces.append(face)
-            locs.append((start_X,start_Y,end_X,end_Y))
-    
-    if len(faces)>0:
-        faces = np.array(faces, dtype="float32")
-        preds= maskModel.predict(faces)
-    
-    return (locs,preds)
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ['png', 'jpg', 'jpeg']
+#new_user = User(username='zhangsan', password='123456')
 
-@app.route('/', methods=['GET'])
-def main_page():
-    return render_template('homepage.html')
+with app.app_context():
+    db.create_all()
 
-@app.route('/', methods=['POST'])
-def predict():
-    if 'imagefile' not in request.files:
-        flash('No file part')
-        return redirect(url_for('main_page'))
-    imagefile = request.files['imagefile']
-    filesavepath='online_detector_uploads/'+secure_filename(imagefile.filename)
-    error=False
-    if imagefile and allowed_file(imagefile.filename):
-        filename = secure_filename(imagefile.filename)
-        imagefile.save(filesavepath)
-        image= cv2.imread(filesavepath)
-        (h,w)= image.shape[:2]
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
-        blob= cv2.dnn.blobFromImage(image,1.0,(300,300),(104.0,177.0,123.0))#normalization
+@app.route('/user/add')
+def user_add():
+    new_user = User(username='zhangsan', password='123456')
+    db.session.add(new_user)
+    db.session.commit()
+    return 'add user success'
 
-        #computing face detection
-        net.setInput(blob)
-        detections= net.forward()
-        for i in range(0,detections.shape[2]):
-            confidence= detections[0,0,i,2]
-            if confidence>0.5:
-                box= detections[0,0,i,3:7]*np.array([w,h,w,h])
-                (start_X,start_Y, end_X, end_Y)= box.astype('int')
-                (start_X,start_Y)= (max(0,start_X),max(0,start_Y))
-                (end_X,end_Y)= (min(w-1,end_X),min(h-1,end_Y))
-                face= image[start_Y:end_Y, start_X:end_X]
-                face= cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
-                face= cv2.resize(face, (224,224))
-                face= img_to_array(face)
-                face= preprocess_input(face)
-                face= np.expand_dims(face, axis=0)
+@app.route('/user/query')
+def user_query():
+    user1=User.query.get(1)
+    print("user id is {}, username is {}, password is {}".format(user1.id, user1.username, user1.password))
+    users=User.query.filter_by(username='zhangsan')
+    for user in users:
+        print("user id is {}, username is {}, password is {}".format(user.id, user.username, user.password))
+    return 'query user success'
 
-                (with_mask,without_mask)= model.predict(face)[0]
-                if with_mask>without_mask:
-                    label="Mask"
-                    color= (0,255,0) 
-                else:
-                    label="No Mask"
-                    color= (0,0,255)
+@app.route('/user/update')
+def user_update():
+    user=User.query.filter_by(username='zhangsan').first()
+    user.password='222222'
+    db.session.commit()
+    return 'update user success'
 
-                label="{}:{:.2f}%".format(label, max(with_mask,without_mask)*100) 
-                cv2.putText(image,label,(start_X,start_Y-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
-                cv2.rectangle(image, (start_X,start_Y),(end_X,end_Y),color,2)
-        outputpath=os.path.join("static/online_detector_results",filename)
-        cv2.imwrite(outputpath, image)
-        return render_template('homepage.html', outputImage=filename,error=error)
-    else:
-        error=True
-        return render_template('homepage.html', error=error)
-
-@app.route('/download/<path:filename>', methods=['GET'])
-def download(filename):
-    # Returning file from appended path
-    return send_file( os.path.join('static/online_detector_results',filename), as_attachment=True)
-
-@app.route('/streaming')
-def streaming():
-    return render_template('streaming.html')
-
-
-#video streaming
-
-video = cv2.VideoCapture(0)
-
-def gen_frames():
-    
-    while True:
-        success, image = video.read()
-        if not success:
-            break
-        else:
-            (oheight, owidth) = image.shape[:2]
-            adjusted_width = 400
-            adjusted_height = int(adjusted_width * oheight / owidth)
-            cv2.imshow("image", image)
-            image = cv2.resize(image, (adjusted_width, adjusted_height), interpolation = cv2.INTER_AREA)
-            (locs,preds)=detect_and_predict_mask(image,net,model)
-
-            for (loc,pred) in zip(locs,preds):
-                    (start_X,start_Y,end_X,end_Y)= loc
-                    (with_mask,without_mask)= pred
-                    if with_mask>without_mask:
-                        label="Mask"
-                        color= (0,255,0) 
-                    else:
-                        label="No Mask"
-                        color= (0,0,255)
-
-                    label="{}:{:.2f}%".format(label, max(with_mask,without_mask)*100) 
-                    cv2.putText(image,label,(start_X,start_Y-10),cv2.FONT_HERSHEY_SIMPLEX,0.45,color,2)
-                    cv2.rectangle(image, (start_X,start_Y),(end_X,end_Y),color,2)
-
-            ret, jpeg = cv2.imencode('.jpg', image)
-            frame = jpeg.tobytes()
-            
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/user/delete')
+def user_delete():
+    user=User.query.filter_by(username='zhangsan').first()
+    db.session.delete(user)
+    db.session.commit()
+    return 'delete user success'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT'), threaded=True)
+    app.run(debug = True)
